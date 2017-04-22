cd29c51ad787ba115df142264a1f311a1abe6a52
Diego Islas Ocampo <hyuchia@gmail.com>
2017-04-22 00:03:17 -0500

    Add basic server functions

--- src/main.c
+++ src/main.c
@@ -1,6 +1,6 @@
 /* main.c
  *
- * Copyright (C) 2017 Diego Islas Ocampo
+ * Copyright (C) 2017 Aegis Framework
  *
  * Permission is hereby granted, free of charge, to any person obtaining
  * a copy of this software and associated documentation files (the
@@ -23,12 +23,139 @@
  */
 
 #include <gtk/gtk.h>
-
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netdb.h>
+#include <string.h>
+#include <arpa/inet.h>
+#include <stdlib.h>
+#include <sys/stat.h>
+#include <unistd.h>
 #include "text.h"
 
+#define SIZE 8
+#define MSGSIZE 1024
 
 GtkTextBuffer *buffer;
 GtkTextIter iter;
+
+void writeLog (char* str) {
+	if (gtk_text_buffer_get_char_count (buffer) > 0) {
+		gtk_text_buffer_get_end_iter(buffer , &iter);
+		gtk_text_buffer_insert (buffer, &iter, "\n", -1);
+		gtk_text_buffer_get_end_iter(buffer , &iter);
+		gtk_text_buffer_insert (buffer, &iter, str, strlen(str));
+
+	} else {
+		gtk_text_buffer_set_text (buffer, str, strlen(str));
+	}
+
+}
+
+int readLine(int s, char *line, int *result_size) {
+    int acum=0, size;
+    char buffer[SIZE];
+
+    while( (size=read(s, buffer, SIZE)) > 0) {
+        if (size < 0) return -1;
+        strncpy(line+acum, buffer, size);
+        acum += size;
+        if(line[acum-1] == '\n' && line[acum-2] == '\r') {
+            break;
+        }
+    }
+
+    *result_size = acum;
+
+    return 0;
+}
+
+int writeLine(int s, char *line, int total_size) {
+    int acum = 0, size;
+    char buffer[SIZE];
+
+    if(total_size > SIZE) {
+        strncpy(buffer, line, SIZE);
+        size = SIZE;
+    } else  {
+        strncpy(buffer, line, total_size);
+        size = total_size;
+    }
+
+    while( (size=write(s, buffer, size)) > 0) {
+        if(size<0) return size;
+        acum += size;
+        if (acum >= total_size) break;
+
+        size = ((total_size-acum)>=SIZE)?SIZE:(total_size-acum)%SIZE;
+        strncpy(buffer, line+acum, size);
+    }
+
+    return 0;
+}
+
+int serve(int s) {
+	printf("1");
+
+    char command[MSGSIZE];
+    int size, r, nlc = 0, fd, read_bytes;
+
+    // Lee lo que pide el cliente
+    while(1) {
+        r = readLine(s, command, &size);
+        command[size-2] = 0;
+        size-=2;
+        writeLog(command);
+
+
+		// Reached end of the Request Header
+        if(command[size-1] == '\n' && command[size-2] == '\r') {
+            break;
+        }
+		// Esto esta mal mal mal
+		if(strlen(command) == 0) {
+			break;
+		}
+    }
+    sleep(1);
+
+    sprintf(command, "HTTP/1.0 200 OK\r\n");
+    writeLine(s, command, strlen(command));
+
+    sprintf(command, "Date: Fri, 31 Dec 1999 23:59:59 GMT\r\n");
+    writeLine(s, command, strlen(command));
+
+    sprintf(command, "Content-Type: image/jpeg\r\n");
+    writeLine(s, command, strlen(command));
+
+    sprintf(command, "Content-Length: 29936\r\n");
+    writeLine(s, command, strlen(command));
+
+    sprintf(command, "\r\n");
+    writeLine(s, command, strlen(command));
+
+    FILE *fin = fopen("mainiso_forcampus.jpg", "r");
+	FILE *fout = fdopen(s, "w");
+
+	struct stat buf;
+
+	/*stat("mainiso?forcampus.jpg", &buf);
+	printf("Size -----------> %ld\n", buf.st_size);
+
+	char file[32*1024];
+	int suma = 0;
+	size = fread(file, 1, 29936, fin);
+	printf("Archivo: %d\n", size);
+
+    while( (size=write(s, &file[suma], MSGSIZE)) > 0) {
+		suma += size;
+		if (suma >= 29936) break;
+	}*/
+    sync();
+}
+
 
 static void activate (GtkApplication *app, gpointer user_data) {
 	GtkWidget *window;
